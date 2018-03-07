8ef199e4b471cd69cae996a93dbefeb42ec574a1
root <root@localhost.localdomain>
2018-03-07 16:51:24 +0800

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<h1>im test</h1>
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		
+	}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import HelloVue from '@/components/HelloVue'
+import test from '@/components/test'
+import HelloIview from '@/components/HelloIview'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloIview
+    },
+    {
+    	path: '/foo',
+    	name: 'HelloVue',
+    	component: HelloVue
+    },
+    {
+    	path: '/iview',
+    	name: 'HelloIview',
+    	component: HelloIview,
+    	children: [
+    		{
+    			path: '/iview/test',
+    			component: test
+    		},
+    		{
+    			path: '/iview/hey',
+    			component: HelloIview
+    		}
+    	]
+    }
+  ]
+})
